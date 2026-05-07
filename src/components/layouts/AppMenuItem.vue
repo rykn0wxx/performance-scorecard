7a936d808d85a70d2f7cff7bd9da699ef3053123
r4ur1c1 <r4ur1c1@gmail.com>
2026-05-07 21:43:28 +0000

    asdasd

--- src/components/layouts/AppMenuItem.vue
+++ src/components/layouts/AppMenuItem.vue
@@ -15,7 +15,7 @@
     </router-link>
     <Transition v-if="item.children && item.visible !== false" name="layout-submenu">
       <ul v-show="root ? true : isActive" class="layout-submenu">
-        <AppMenuItem v-for="child in item.children" :key="child.label + '_' + (child.to || child.path)" :item="child" :root="false" :parentPath="fullPath" />
+        <AppMenuItem v-for="child in item.children" :key="child.label + '_' + (child.to || child.pathRef)" :item="child" :root="false" :parentPath="fullPath" />
       </ul>
     </Transition>
   </li>
@@ -45,11 +45,11 @@ const props = defineProps({
   }
 })
 
-const fullPath = computed(() => (props.item.path ? (props.parentPath ? props.parentPath + props.item.path : props.item.path) : null))
+const fullPath = computed(() => (props.item.pathRef ? (props.parentPath ? props.parentPath + props.item.pathRef : props.item.pathRef) : null))
 
 const isActive = computed(() => {
   // return props.item.pathRef ? layoutState.activePath?.startsWith(fullPath.value) : layoutState.activePath === props.item.to
-  return props.item.path ? layoutState.activePath?.startsWith(fullPath.value) : layoutState.activePath === props.item.to
+  return props.item.pathRef ? layoutState.activePath?.startsWith(fullPath.value) : layoutState.activePath === props.item.to
 })
 
 const hasActiveItem = computed(() => props.item.children && !!props.item.children.find(itm => route.path.startsWith(itm.to)))
@@ -66,7 +66,7 @@ function itemClick(event, item) {
 
   if (item.children) {
     if (isActive.value) {
-      layoutState.activePath = layoutState.activePath.replace(item.path, '')
+      layoutState.activePath = layoutState.activePath.replace(item.pathRef || item.to, '')
     } else {
       layoutState.activePath = fullPath.value
       layoutState.menuHoverActive = true
